9b19acced12f3c973ad283159916e2135193da07
lee cheong woo <86641797+Nangnaa@users.noreply.github.com>
2021-09-27 17:54:42 +0900

    09 27 17:54

--- HTML/js/naver_join.js
+++ HTML/js/naver_join.js
@@ -1,17 +1,28 @@
 const join_id = document.querySelector('#id');
-const join_submit = document.querySelector('.join_subnit');
+const join_submit = document.querySelector('.join_submit');
+const warning_requireds = document.querySelectorAll('.text_required');
+const idCheck_false = document.querySelector('.idCheck_false');
 
 join_id.onblur = () => {
-    alret(join_id.value + '는 사용 가능한 아이디 입니다.' )
+    let test_id = 'junil1234';
+    if(join_id.length == 0){
+        warning_requireds[0].style.display = 'block';
+    }else{
+        warning_requireds[0].style.display = 'none';
+        if(join_id.value == test_id){
+            idCheck_false[0].style.display = 'block';
+        }else{
+            idCheck_false[0].style.display = 'none';
+        }
+    }
 }
 
-join_submit.onclick = () =>{
-    alert(join_id.value + '계정을 생성하였습니다.')
+join_submit.onclick = () => {
+    alert(join_id.value + ' 계정을 생성하였습니다.')
     print();
 }
 
 const long_text = document.getElementsByClassName('long_text');
-//해당하는 모든 객체를 가져온다.
 console.log(long_text[0].value);
 console.log(long_text[1].value);
 console.log(long_text[2].value);
@@ -19,17 +30,17 @@ console.log(long_text[3].value);
 long_text[4].style.display = 'none';
 
 const id_text = document.getElementById('id');
-id_text.style.display = ' block'
-id_text.value = 'cjddn2519';
+id_text.style.display = 'block';
+id_text.value = 'junil1234';
 
-const inputs = document.getElementById('input');
+const inputs = document.getElementsByTagName('input');
 console.log(inputs);
 
-const long_text2 = document.querySelectorAll('.long_text')
+const long_text2 = document.querySelectorAll('.long_text');
 // const long_text = document.getElementsByClassName('long_text'); 위와 동일 클래스는 . 사용
-const id_text2 = document.querySelector('#id')
+const id_text2 = document.querySelector('#id');
 // const id_text = document.getElementById('id'); 아이디는 #사용 
-const inputs2 = document.querySelectorAll('input')
+const inputs2 = document.querySelectorAll('input');
 // const inputs = document.getElementById('input'); All은 모두 가져온다는뜻
 
 console.log(long_text2);
